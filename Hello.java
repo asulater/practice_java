6f618cae4e04ff98061730a454d3164c21565373
asulater <asulater@gmail.com>
2022-09-26 21:58:58 +0900

    updates

--- Hello.java
+++ Hello.java
@@ -1,18 +1,6 @@
 public class Hello {
     public static void main(String args[]){
-        int a1, a2, a3;
-        int total;
-        float avg;
 
-        a1 = 50;
-        a2 = 60;
-        a3 = 70;
-
-        total = a1+a2+a3;
-        avg = total/3;
-
-        System.out.printf("total is %d\n", total);
-        System.out.printf("avg is %f\n", avg);
 
     }
 }
