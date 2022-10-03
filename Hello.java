ef2dfe0dad2149c0de6e8ed5aa38fe27d9ed5729
asulater <asulater@gmail.com>
2022-10-04 01:54:24 +0900

    updates

--- Hello.java
+++ Hello.java
@@ -1,8 +1,29 @@
 public class Hello {
     public static void main(String args[]){
-        System.out.println("Hello \"World");
-        System.out.println("5 * 3");
-        System.out.println(5*3);
-        System.out.println((24*60) * 60);
+       int a = 3;
+       int b = 7;
+       int c = 2;
+       int d = 1;
+
+       if(a+b > c+d) {
+           System.out.println("a+b is grater then c + d");
+       }
+
+       int angle1 = 30;
+       int angle2 = 50;
+       int angle3 = 100;
+
+       if(angle1 + angle2 + angle3 == 180) {
+           System.out.println("this three of angles is 180");
+       }
+
+       int test = 120;
+
+       if(test % 2 ==0) {
+           System.out.println("test var is even number");
+       }
+       else {
+           System.out.println("test var is not even number");
+       }
     }
 }
